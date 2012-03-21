648f37ad6f31be4a54daffd8f13d050fe89f1736
Gabriele Cocco <gabriele@gabrielecocco.it>
2012-03-20 19:41:47 -0700

    Added tester application

---
+++ Tester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GPUPerfNet;
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GPA_Status status = GPUPerfCore.GPA_Initialize();
+            status = GPUPerfCore.GPA_Destroy();
+
+        }
+    }
+}
